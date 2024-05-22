a8f8e474881daa3a4c94af787ca02f2986ce69a9
Karol <weber.karol.86@gmail.com>
2024-05-22 11:34:16 +0200

    extract url and login action

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,15 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Login', () => {
+  test.beforeEach(async ({ page }) => {
+    const url = 'https://demo-bank.vercel.app/';
+    await page.goto(url);
+  })
+
   test('Correct credentials', async ({ page }) => {
     // Arrange
-    const url = 'https://demo-bank.vercel.app/';
     const userLogin = 'testuser';
     const userPassword = 'test1234';
     const userName = 'Jan Demobankowy';
 
     // Act
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userLogin);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
@@ -20,12 +23,10 @@ test.describe('Login', () => {
 
   test('Inorrect credentials -> Invalid username', async ({ page }) => {
     // Arrange
-    const url = 'https://demo-bank.vercel.app/';
     const userLogin = 'test';
     const errorLogin = 'identyfikator ma min. 8 znaków';
 
     // Act
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userLogin);
     await page.getByTestId('login-input').blur();
 
@@ -35,12 +36,10 @@ test.describe('Login', () => {
 
   test('Inorrect credentials -> No password', async ({ page }) => {
     // Arrange
-    const url = 'https://demo-bank.vercel.app/';
     const userLogin = 'testuser';
     const errorLogin = 'pole wymagane';
 
     // Act
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userLogin);
     await page.getByTestId('password-input').click();
     await page.getByTestId('password-input').blur();
@@ -51,13 +50,11 @@ test.describe('Login', () => {
 
   test('Inorrect credentials -> Invalid password', async ({ page }) => {
     // Arrange
-    const url = 'https://demo-bank.vercel.app/';
     const userLogin = 'testuser';
     const userPassword = 'test';
     const errorLogin = 'hasło ma min. 8 znaków';
 
     // Act
-    await page.goto(url);
     await page.getByTestId('login-input').fill(userLogin);
     await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('password-input').blur();
--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,22 +1,23 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Pulpit', () => {
-  test('Fast transfer', async ({ page }) => {
-    // Arrange
+  test.beforeEach(async ({ page }) => {
     const url = 'https://demo-bank.vercel.app/';
     const userLogin = 'testuser';
     const userPassword = 'test1234';
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userLogin);
+    await page.getByTestId('password-input').fill(userPassword);
+    await page.getByTestId('login-button').click();
+  })
+  test('Fast transfer', async ({ page }) => {
+    // Arrange
     const revicer = '2';
     const amount = '150';
     const title = 'Transfer';
     const message = `Przelew wykonany! Chuck Demobankowy - ${amount},00PLN - ${title}`;
 
     // Act
-    await page.goto(url);
-    await page.getByTestId('login-input').fill(userLogin);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
-
     await page.locator('#widget_1_transfer_receiver').selectOption(revicer);
     await page.locator('#widget_1_transfer_amount').fill(amount);
     await page.locator('#widget_1_transfer_title').fill(title);
@@ -30,19 +31,11 @@ test.describe('Pulpit', () => {
 
   test('Phone top up', async ({ page }) => {
     // Arrange
-    const url = 'https://demo-bank.vercel.app/';
-    const userLogin = 'testuser';
-    const userPassword = 'test1234';
     const revicer = '500 xxx xxx';
     const amount = '150';
     const message = `Doładowanie wykonane! ${amount},00PLN na numer ${revicer}`;
 
     // Act
-    await page.goto(url);
-    await page.getByTestId('login-input').fill(userLogin);
-    await page.getByTestId('password-input').fill(userPassword);
-    await page.getByTestId('login-button').click();
-
     await page.locator('#widget_1_topup_receiver').selectOption(revicer);
     await page.locator('#widget_1_topup_amount').fill(amount);
     await page.locator('#uniform-widget_1_topup_agreement span').click();
